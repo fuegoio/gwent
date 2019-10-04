ca2ea36d663e40256963c10fc283b995bf7ac048
Alexis Tacnet <alexistacnet@gmail.com>
2019-10-04 11:36:24 +0200

    Add favicon and improve front-end

--- front/src/components/GameFinder.vue
+++ front/src/components/GameFinder.vue
@@ -1,32 +1,59 @@
 <template>
-    <v-layout class="full-height" row>
-        <v-flex class="full-height" style="margin-top: auto; margin-bottom: auto" xs4>
-            <v-container id="side-bar">
-                <v-text-field v-model="username" placeholder="Username" outlined dark hide-details :disabled="registered"></v-text-field>
-                <v-btn block style="margin-top: 20px" :disabled="username.length == 0" v-if="!registered" @click="sign_in">Connect</v-btn><br><br>
+    <div class="game-finder">
+        <v-row class="fill-height" align="center" justify="center">
+            <v-col cols="4" class="pa-5 mt-3" offset="1">
+                <v-text-field v-model="username" placeholder="Username" outlined dark hide-details
+                              :disabled="registered">
+                    <template v-slot:append>
+                        <v-fade-transition mode="out-in">
+                            <v-progress-circular
+                                    v-if="loading"
+                                    size="24"
+                                    width="2"
+                                    color="orange"
+                                    indeterminate
+                            ></v-progress-circular>
+                            <v-icon v-else-if="registered" color="green">
+                                check
+                            </v-icon>
+                        </v-fade-transition>
+                    </template>
+                </v-text-field>
+
+                <v-btn block style="margin-top: 20px" :disabled="username.length === 0" v-if="!registered"
+                       @click="sign_in">Connect
+                </v-btn>
+
                 <v-card
-                  style="background-color: #3F3632; border-radius: 15px"
-                  class="mx-auto"
-                  v-if="registered && users.length > 0"
+                        style="background-color: #3F3632; border-radius: 15px"
+                        v-if="registered && users.length > 1"
                 >
-                  <v-list-item two-line v-for="user in users" class="list-item" v-if="sid != user['id']">
-                    <v-list-item-content>
-                      <v-list-item-title>{{user['username']}}</v-list-item-title>
-                      <v-list-item-subtitle>{{user['available'] ? 'Available' : 'In game'}}</v-list-item-subtitle>
-                    </v-list-item-content>
-                    <v-btn rounded :disabled="!user['available']" @click="propose_game(user)">PLay</v-btn>
-                  </v-list-item>
+                    <v-list-item two-line v-for="user in users" class="list-item" v-if="sid !== user['id']">
+                        <v-list-item-content>
+                            <v-list-item-title>{{user['username']}}</v-list-item-title>
+                            <v-list-item-subtitle>{{user['available'] ? 'Available' : 'In game'}}
+                            </v-list-item-subtitle>
+                        </v-list-item-content>
+                        <v-btn rounded :disabled="!user['available']" @click="propose_game(user)">PLay</v-btn>
+                    </v-list-item>
                 </v-card>
-                <p v-if="!registered" style="text-align: center; color: #05DC95;">Enter a username to access matchmaking</p>
-                <p v-if="registered && users.length == 0" style="text-align: center; color: #05DC95;">No users logged in</p>
-            </v-container>
-        </v-flex>
-        <v-flex class="full-height" xs8>
-            <img src="gwent_logo.gif" id="logo">
-        </v-flex>
+                <p style="text-align: center; color: #05DC95;"
+                   v-else-if="registered && users.length === 1" class="mt-3">
+                    No users logged in
+                </p>
+                <p style="text-align: center; color: #05DC95;" v-else class="mt-3">
+                    Enter a username to access matchmaking
+                </p>
+            </v-col>
+            <v-col cols="7">
+                <v-img src="gwent_logo.gif" id="logo"/>
+            </v-col>
+        </v-row>
+
         <v-dialog v-model="game_proposal" max-width="30%">
             <v-card v-if="!proposer">
-                <p style="text-align: center; padding: 15px; margin: 0" class="title">{{adversary['username']}} wants to play with you!</p>
+                <p style="text-align: center; padding: 15px; margin: 0" class="title">{{adversary['username']}} wants to
+                    play with you!</p>
                 <v-card-actions>
                     <div class="flex-grow-1"></div>
                     <v-btn color="#05DC95" text @click="refuse_game">
@@ -39,29 +66,29 @@
             </v-card>
             <v-card v-if="proposer">
                 <v-container>
-                    <p style="text-align: center; padding: 10px" class="title">Waiting for {{adversary['username']}} to accept your invitation</p>
+                    <p style="text-align: center; padding: 10px" class="title">Waiting for {{adversary['username']}} to
+                        accept your invitation</p>
                     <v-progress-circular
                             style="margin-left: 40%; margin-right: 40%; width: 20%; margin-bottom: 30px"
-                        indeterminate
-                        color="#05DC95"
+                            indeterminate
+                            color="#05DC95"
                     ></v-progress-circular>
                 </v-container>
             </v-card>
         </v-dialog>
-        <v-snackbar
-            v-model="snackbar"
-            :timeout="2000"
-        >
+
+        <v-snackbar v-model="snackbar" :timeout="2000">
             Game proposal refused
-        <v-btn color="#05DC95" text @click="snackbar = false">Close</v-btn>
-      </v-snackbar>
-    </v-layout>
+            <v-btn color="#05DC95" text @click="snackbar = false">Close</v-btn>
+        </v-snackbar>
+    </div>
 </template>
 
 <script>
     export default {
         name: "GameFinder",
         data: () => ({
+            loading: false,
             username: "",
             sid: "",
             registered: false,
@@ -72,46 +99,48 @@
             snackbar: false
         }),
         sockets: {
-            connect(){
-                this.sid = this.$socket.id
+            connect() {
+                this.sid = this.$socket.id;
             },
             available_players(data) {
-                console.log(data)
-                if (data['registered'] == this.sid) {
+                if (data['registered'] === this.sid) {
                     this.registered = true
                 }
-                this.users = data['available_users']
-                console.log(this.users)
+                this.users = data['available_users'];
+                setTimeout(() => {
+                    this.loading = false
+                }, 1000);
             },
             game_proposal(data) {
-                this.adversary = data['player'][0]
+                this.adversary = data['player'][0];
                 this.game_proposal = true
             },
             game_refused() {
-                this.game_proposal = false
+                this.game_proposal = false;
                 this.snackbar = true
             }
         },
         methods: {
             sign_in() {
-                this.$socket.emit('register', {'username': this.username})
+                this.loading = true;
+                this.$socket.emit('register', {'username': this.username});
             },
             propose_game(adversary) {
-                console.log('Sending game proposal to ' + adversary['id'])
-                this.game_proposal = true
-                this.proposer = true
-                this.adversary = adversary
+                console.log('Sending game proposal to ' + adversary['id']);
+                this.game_proposal = true;
+                this.proposer = true;
+                this.adversary = adversary;
                 this.$socket.emit('propose_game', {adversary_id: adversary['id']})
             },
             launch_game() {
-                this.$router.push('/game')
-                this.game_proposal = true
-                const faction1 = ['northern', 'nilfgaardian', 'scoiatael', 'monster'][Math.floor(Math.random() * Math.floor(4))]
-                const faction2 = ['northern', 'nilfgaardian', 'scoiatael', 'monster'][Math.floor(Math.random() * Math.floor(4))]
+                this.$router.push('/game');
+                this.game_proposal = true;
+                const faction1 = ['northern', 'nilfgaardian', 'scoiatael', 'monster'][Math.floor(Math.random() * Math.floor(4))];
+                const faction2 = ['northern', 'nilfgaardian', 'scoiatael', 'monster'][Math.floor(Math.random() * Math.floor(4))];
                 this.$socket.emit('launch_game', {name1: this.username, faction1: faction1, name2: this.adversary['username'], faction2: faction2})
             },
             refuse_game() {
-                this.game_proposal = false
+                this.game_proposal = false;
                 this.$socket.emit('refuse_game', {refuser: this.sid, proposer: this.adversary['id']})
             }
         }
@@ -119,19 +148,15 @@
 </script>
 
 <style scoped>
-    .full-height {
+    .game-finder {
         height: 100%;
     }
+
     #logo {
-        position: absolute;
-        top: 0;
-        bottom: 0;
         margin: auto;
+        display: block;
     }
-    #side-bar {
-        padding: 20px;
-        margin-top: 20%;
-    }
+
     .list-item {
         background-color: #05DC95;
         border-radius: 15px;
