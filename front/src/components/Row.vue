02d7edc69bd53134845289f1ab3424877751cdf6
Tanguy Moullec <tanguy.moullec@gmail.com>
2019-10-14 17:53:57 +0200

    Fixed mulligan css issue

--- front/src/components/Row.vue
+++ front/src/components/Row.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <v-col v-if="!description" cols="12" style="height: 125px; padding: 0">
+        <v-col v-if="!description" cols="12" class="row">
             <v-row justify="center">
                 <v-card v-for="card in cards" style="background-color: #3F3632; border-radius: 14px;"
                         :disabled="disabled" @click="click(card['id'])">
@@ -10,13 +10,11 @@
                 </v-card>
             </v-row>
         </v-col>
-        <v-col v-if="description" cols="12">
+        <v-col cols="12" v-if="description" class="description_row">
             <v-row justify="center">
-                <v-card v-for="card in cards" style="background-color: #3F3632; border-radius: 14px;"
-                        :disabled="disabled" @click="click(card['id'])">
-                    <figure id="with_description">
-                        <v-img :src="'./cards/' + card['img_name'] + '.png'"></v-img>
-                    </figure>
+                <v-card v-for="card in cards" style="background-color: #3F3632" v-on:click="click(card['id'])"
+                        :disabled="disabled">
+                    <v-img id="card" :src="'./cards/' + card['img_name'] + '.png'"></v-img>
                 </v-card>
             </v-row>
         </v-col>
@@ -45,10 +43,18 @@
         border-radius: 12px;
     }
 
-    #with_description {
-        width: 130px;
-        overflow: hidden;
-        margin: 0;
-        border-radius: 18px;
+    #card {
+        max-height: 300px;
+        max-width: 150px;
+    }
+
+    .row {
+        height: 125px;
+        padding: 0;
+        border-color: coral;
+    }
+
+    .description_row {
+        height: 300px;
     }
 </style>
