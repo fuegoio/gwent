8654f00b4696a362af3520920baf448c3138b028
Tanguy Moullec <tanguy.moullec@gmail.com>
2019-10-20 20:00:57 +0200

    Refactoring player info

--- front/src/components/PlayerCard.vue
+++ front/src/components/PlayerCard.vue
@@ -1,14 +1,27 @@
 <template>
-    <div style="background-color: blue">
-        <v-row style="background-color: red">
-            <v-img :src="'./cards/' + faction_to_image[player['faction']] + '.png'"></v-img>
-        </v-row>
-        <v-row>
-            {{player['name']}}
-            {{player['faction']}}
-            {{player['lives']}}
-        </v-row>
-    </div>
+    <v-row>
+        <v-col style="margin: 0; padding: 0">
+            <figure class="card">
+                <v-img v-if="player['faction']" :src="'./cards/' + faction_to_image[player['faction']] + '.png'"></v-img>
+            </figure>
+        </v-col>
+        <v-col>
+            <v-row style="margin: 0">
+                <v-col style="padding: 0">
+                    <v-img v-if="player['lives'] == 2" src="ruby.png" class="ruby"></v-img>
+                    <v-img v-if="player['lives'] == 1" src="ruby-grey.png" class="ruby"></v-img>
+                </v-col>
+                <v-col style="padding: 0">
+                    <v-img v-if="player['lives'] > 0" src="ruby.png" class="ruby"></v-img>
+                    <v-img v-if="player['lives'] == 0" src="ruby-grey.png" class="ruby"></v-img>
+                </v-col>
+            </v-row>
+            Username: {{player['name']}}<br>
+            Faction: {{player['faction']}}<br>
+
+
+        </v-col>
+    </v-row>
 </template>
 
 <script>
@@ -19,7 +32,7 @@
         data: () => ({
             faction_to_image: {
                 northern: 'foltest_forged',
-                nilfgaardian: '',
+                nilfgaardian: 'emhyr_imperial',
                 scoiatael: 'francesca_beautiful',
                 monster: 'eredin_bringer',
             }
@@ -28,5 +41,15 @@
 </script>
 
 <style scoped>
-
+    .card {
+        width: 180px;
+        height: 250px;
+        overflow: hidden;
+        margin: 0;
+        border-radius: 12px;
+    }
+    .ruby {
+        height: 50px;
+        width: 50px;
+    }
 </style>
